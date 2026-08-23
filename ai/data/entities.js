4970ae994954e974591e012ff058684905f45a02
reinsangma-jpg <reinsangma@gmail.com>
2026-08-23 23:55:47 +0530

    Add entities.js with geographical and cultural data

---
+++ ai/data/entities.js
@@ -0,0 +1,12 @@
+const entities = {
+  "Assam": ["state of assam"],
+  "Dispur": ["assam capital", "dispur assam"],
+  "Guwahati": ["gauhati"],
+  "Brahmaputra": ["brahmaputra river"],
+  "Kaziranga National Park": ["kaziranga"],
+  "Majuli": ["majuli island"],
+  "India": ["republic of india", "bharat"],
+  "Monsoon": ["monsoon season"],
+  "JavaScript": ["js"]
+};
+export default entities;
