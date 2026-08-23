abfb213a1516e8b747631a481ee4406da9bd7b34
reinsangma-jpg <reinsangma@gmail.com>
2026-08-23 23:53:13 +0530

    Add corrections object for common misspellings

---
+++ ai/data/corrections.js
@@ -0,0 +1,18 @@
+const corrections = {
+  "captial": "capital",
+  "capitol": "capital",
+  "assamesee": "assamese",
+  "geograpy": "geography",
+  "climatologyy": "climatology",
+  "enviroment": "environment",
+  "goverment": "government",
+  "technlogy": "technology",
+  "programing": "programming",
+  "kazirangae": "kaziranga",
+  "dispurh": "dispur",
+  "brahmaptra": "brahmaputra",
+  "monsooon": "monsoon",
+  "temprature": "temperature",
+  "humidty": "humidity"
+};
+export default corrections;
